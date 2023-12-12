87e17c428c547bc2a4fd678c03adbc6fb24c0581
chimmimmi <145756968+chimmimmi@users.noreply.github.com>
2023-12-12 10:39:38 +0900

    2023-12-12 (10:39)
     교안 챕터7 까지 작성 (게시글 등록 처리 진행)
    1) 'InsuranceController' 의 'openInsuranceWrite' 메서드 내용 변경
    2) write.html 내용 변경 (1/3)
    3) 'InsuranceController' 의 'registerInsurance' 메서드 생성
    4) localhost:8080/insurance/write.do 에서 게시글 작성 후 저장하기
    5) 데이터베이스에 저장은 되었으나 화면에서는 오류로 뜸 (Whitelabel Error Page) 추후 교안에서 작업할 예정

--- src/main/java/com/insurance/controller/InsuranceController.java
+++ src/main/java/com/insurance/controller/InsuranceController.java
@@ -1,10 +1,14 @@
 package com.insurance.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.insurance.domain.InsuranceDTO;
 import com.insurance.service.InsuranceService;
 
 @Controller
@@ -14,30 +18,40 @@ public class InsuranceController
 	private InsuranceService insuranceService;
 
 	@GetMapping(value = "/insurance/write.do")
-	public String openInsuranceWrite(Model model)
+	public String openInsuranceWrite(@RequestParam(value = "idx", required = false) Long idx, Model model)
 	{
-		String ins_type = "보험종목";
-		String ins_co = "보험회사";
-		String contract_date = "계약체결일";
-		String contract_no = "계약번호";
-		String contractor = "계약자";
-		String insured = "피보험자";
-		String prod_name = "상품명";
-		String payt_exp = "납&만기";
-		int premium = 1000;
-		String memo_col = "메모란";
-		
-		model.addAttribute("it", ins_type);
-		model.addAttribute("ic", ins_co);
-		model.addAttribute("cd", contract_date);
-		model.addAttribute("cn", contract_no);
-		model.addAttribute("c",  contractor);
-		model.addAttribute("i",  insured);
-		model.addAttribute("pn", prod_name);
-		model.addAttribute("pe", payt_exp);
-		model.addAttribute("p", premium);
-		model.addAttribute("m", memo_col);
-		
+		if (idx == null)
+		{
+			model.addAttribute("insurance", new InsuranceDTO());
+		} else {
+			InsuranceDTO insurance = insuranceService.getInsuranceDetail(idx);
+			
+			if (insurance == null)
+			{
+				return "redirect:/insurance/list.do";
+			}
+			model.addAttribute("insurance", insurance);
+		}
 		return "insurance/write";
 	}
+	
+	@PostMapping(value = "/insurance/register.do")                // 작성한 글을 저장하게 해주는 컨트롤러?
+	public String registerInsurance(final InsuranceDTO params)
+	{
+		try
+		{
+			boolean isRegistered = insuranceService.registerInsurance(params);
+			
+			if (isRegistered == false)
+			{
+				// TODO => 게시글 등록에 실패하였다는 메시지를 전달한다.
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달한다.
+		} catch (Exception e) {
+			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달한다.
+		}
+		return "redirect:/insurance/list.do";
+	}
 }
+
