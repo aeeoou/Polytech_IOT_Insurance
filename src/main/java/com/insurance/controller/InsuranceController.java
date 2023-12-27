b8cdc429d817063da82d0b1857e0aa78d1b9a7f7
chimmimmi <145756968+chimmimmi@users.noreply.github.com>
2023-12-27 20:43:11 +0900

    2023-12-27 (20:43)
    1. 이전 페이지 정보 유지하기
    (1) 게시글 상세 페이지에서 뒤로 가기 버튼을 클릭했을 때
    (2) 게시글 상세 페이지에서 게시글을 삭제했을 때
    (3) 게시글 수정 페이지에서 뒤로 가기 버튼을 클릭했을 때
    (4) 게시글 수정 페이지에서 게시글 수정이 이루어졌을 때
    신규 게시글 등록은 무조건 1페이지로 리다이렉트 하므로 이전 페이지 정보를 유지할 필요 없음
    
    1-1. InsuranceController.java 의 'openInsuranceDetail' 메서드 내용 변경
    @ModelAttribute를 활용하여 파라미터로 전달받은 객체를 뷰까지 전달
    이전 페이지 정보가 담긴 Criteria 클래스를 상속받는 InsuranceDTO 커맨드 객체를 뷰로 전달
    
    1-2. view.html 의 3개 버튼 th:href 속성 값 변경
    100~103줄 문법
    th:object 속성을 통해 *{} 표현식으로 메서드명과 변수명 만으로 컨트롤러에서 전달받은 객체에 접근 가능
    
    1-3. view.html 의 'deleteInsurance' 함수 변경
    new URLSearchParams 함수
    쿼리 스트링에 포함된 모든 파라미터들을 객체화하는 데 사용
    여기서는 객체화된 queryString에 포함된 파라미터를 순환해서 값이 존재하는 파라미터만 hidden 타입으로 폼에 추가
    
    1-4. UIUtils 클래스에 'getPagingParams' 메서드 추가
    Criteria 클래스의 모든 멤버 변수 (이전 페이지 정보)를 Map에  Key, Value 형태 리턴
    GET 방식이 아닌 POST 방식의 처리에서만 사용
    
    1-5. InsuranceController 의 'deleteInsurance' 메서드 내용 변경
    ModelAttribute :: 이전 페이지 정보가 담겨있는 Criteria 클래스를 상속받는 InsuranceDTO 커맨드 객체를 화면(View)으로 전달
    pagingParams :: UIUtils 클래스에 추가한 getPagingParams 메서드를 호출한다. pagingParams에 담긴 이전 페이지 정보를 showMessageRedirect 메서드의 인자로 전달
    
    1-6. write.html 의 뒤로가기 구문 수정하기
    
    1-7. write.html 의 'registerInsurance' 함수 변경
    
    1-8. InsuranceController 의 'openInsuranceWrite' 메서드 변경
    
    1-9. InsuranceController 의 'registerInsurance' 메서드 변경
    
    * 결과
    1 - 게시글 상세 페이지에서 뒤로 가기 버튼을 클릭했을 때
    2 - 게시글 상세 페이지에서 게시글을 삭제했을 때
    3 - 게시글 수정 페이지에서 뒤로 가기 버튼을 클릭했을 때
    4 - 게시글 수정 페이지에서 게시글 수정이 이루어졌을 때
    URL 에 파라미터가 제대료 연결되어 있는지 꼭 확인하기

--- src/main/java/com/insurance/controller/InsuranceController.java
+++ src/main/java/com/insurance/controller/InsuranceController.java
@@ -1,6 +1,7 @@
 package com.insurance.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -25,7 +26,7 @@ public class InsuranceController extends UiUtils
 	private InsuranceService insuranceService;
 
 	@GetMapping(value = "/insurance/write.do")
-	public String openInsuranceWrite(@RequestParam(value = "idx", required = false) Long idx, Model model)
+	public String openInsuranceWrite(@ModelAttribute("params") InsuranceDTO params, @RequestParam(value = "idx", required = false) Long idx, Model model)
 	{
 		if (idx == null)
 		{
@@ -33,33 +34,35 @@ public class InsuranceController extends UiUtils
 		} else {
 			InsuranceDTO insurance = insuranceService.getInsuranceDetail(idx);
 			
-			if (insurance == null)
+			if (insurance == null || "Y".equals(insurance.getDeleteYn()))
 			{
-				return "redirect:/insurance/list.do";
+				return showMessageWithRedirect("없는 게시글이거나 이미 삭제된 게시글이다.", "/insurance/list.do", Method.GET, null, model);
 			}
 			model.addAttribute("insurance", insurance);
 		}
 		return "insurance/write";
 	}
 	
-	@PostMapping(value = "/insurance/register.do")                // 작성한 글을 저장하게 해주는 컨트롤러?
-	public String registerInsurance(final InsuranceDTO params, Model model)
+	@PostMapping(value = "/insurance/register.do")                           // 작성한 글을 저장하게 해주는 컨트롤러?
+	public String registerInsurance(@ModelAttribute("params") final InsuranceDTO params, Model model)
 	{
+		Map<String, Object> pagingParams = getPagingParams(params);
+		
 		try
 		{
 			boolean isRegistered = insuranceService.registerInsurance(params);
 			
 			if (isRegistered == false)
 			{
-				return showMessageWithRedirect("게시글 등록에 실패하였습니다.", "/insurance/list.do", Method.GET, null, model);				                       
+				return showMessageWithRedirect("게시글 등록에 실패하였습니다.", "/insurance/list.do", Method.GET, pagingParams, model);				                       
 			}
 		} catch (DataAccessException e) {
-			return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, null, model);
+			return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, pagingParams, model);
 			                              
 		} catch (Exception e) {
-			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, null, model);
+			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, pagingParams, model);
 		}
-		return showMessageWithRedirect("게시글 등록이 완료되었습니다.", "/insurance/list.do", Method.GET, null, model);
+		return showMessageWithRedirect("게시글 등록이 완료되었습니다.", "/insurance/list.do", Method.GET, pagingParams, model);
 	}
 	
 	// 게시글 목록 처리 - InsuranceController
@@ -69,27 +72,24 @@ public class InsuranceController extends UiUtils
 		List<InsuranceDTO> insuranceList = insuranceService.getInsuranceList(params);                      // insuranceList :: InsuranceService에서 호출한 getInsuranceList 메서드의 실행 결과물을 저장
 		model.addAttribute("insuranceList", insuranceList); 
 		model.addAttribute("requestURI", request.getRequestURI());
-		System.out.println(request.getRequestURI() + "ㅎㅇㅎㅇ");
 		
 		return "insurance/list";                                                                           // return문 :: 컨트롤러의 리턴 문에 지정된 경로의 HTML이 화면에 출력
 	}
 	
 	// 게시글 조회 처리 - InsuranceController
 	@GetMapping(value = "/insurance/view.do")
-	public String openInsuranceDetail(@RequestParam(value = "idx", required = false) Long idx, Model model)    // RequestParam :: 특정 게시글 조회에 필요한 게시글 번호를 파라미터 전달받는다. required 속성은 파라미터가 필수 값인지에 대한 여부이며 'idx'가 파라미터로 전달되지 않으면 에러 발생 (여기서는 파라미터가 넘어오지 않을 경우에 대해 직접 처리할 예정이므로 'required' 속성을 'false'로 지정)
+	public String openInsuranceDetail(@ModelAttribute("params") InsuranceDTO params, @RequestParam(value = "idx", required = false) Long idx, Model model)    // RequestParam :: 특정 게시글 조회에 필요한 게시글 번호를 파라미터 전달받는다. required 속성은 파라미터가 필수 값인지에 대한 여부이며 'idx'가 파라미터로 전달되지 않으면 에러 발생 (여기서는 파라미터가 넘어오지 않을 경우에 대해 직접 처리할 예정이므로 'required' 속성을 'false'로 지정)
 	{
 		if (idx == null) // if문에서 'idx'가 파라미터로 전달되지 않으면 사용자에게 적절한 메시지를 전달하고, 게시글 리스트로 리다이렉트 (정상적인 경우에는 getInsuranceDetail 메서드의 인자로 idx를 전달해서 게시글 정보를 조회. 만약 없는 게시글이거나 삭제된 게시글이라면 사용자에게 적절한 메시지를 전달하고, 게시글 리스트로 리다이렉트 마지막에는 게시글 정보를 화면(View)으로 전달하고, 게시글 상세 페이지를 리턴)
 		{
-			// TODO => 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
-			return "redirect:/insurance/list.do";
+			return showMessageWithRedirect("올바르지 않은 접근이다.", "/insurance/list.do", Method.GET, null, model);
 		}
 		
 		InsuranceDTO insurance = insuranceService.getInsuranceDetail(idx);
 		
 		if (insurance == null || "Y".equals(insurance.getDeleteYn()))
 		{
-			// TODO => 없는 게시글이거나, 이미 삭제된 게시글이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
-			return "redirect:/insurance/list.do";
+			return showMessageWithRedirect("없는 게시글이거나 이미 삭제된 게시글이다.", "/insurance/list.do", Method.GET, null, model);
 		}
 		model.addAttribute("insurance", insurance);
 		
@@ -98,29 +98,32 @@ public class InsuranceController extends UiUtils
 	
 	// 게시글 삭제 처리 Controller (iot플랫폼실무 챕터10-2p)
 	@PostMapping(value = "/insurance/delete.do")
-	public String deleteInsurance(@RequestParam(value = "idx", required = false) Long idx, Model model)
+	public String deleteInsurance(@ModelAttribute("params") InsuranceDTO params, @RequestParam(value = "idx", required = false) Long idx, Model model)
 	{
 		if (idx == null)
 		{
 			return showMessageWithRedirect("올바르지 않은 접근입니다.", "/insurance/list.do", Method.GET, null, model);
 		}
+	
+		Map<String, Object> pagingParams = getPagingParams(params);
+		
 		try
 		{
 			boolean isDeleted = insuranceService.deleteInsurance(idx);
 			
 			if (isDeleted == false)
 			{
-				return showMessageWithRedirect("게시글 삭제에 실패하였습니다.", "/insurance/list.do", Method.GET, null, model);
+				return showMessageWithRedirect("게시글 삭제에 실패하였습니다.", "/insurance/list.do", Method.GET, pagingParams, model);
 			}
 		
 		} catch (DataAccessException e) {
-			return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, null, model);
+			return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, pagingParams, model);
 		
 		} catch (Exception e) {
-			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, null, model);
+			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/insurance/list.do", Method.GET, pagingParams, model);
 		}
 		
-		return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/insurance/list.do", Method.GET, null, model);
+		return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/insurance/list.do", Method.GET, pagingParams, model);
 	}
 }
 
