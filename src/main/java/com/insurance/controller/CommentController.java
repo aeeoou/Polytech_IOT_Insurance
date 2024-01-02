46905e199c34b0128e56c7a6633d3ab8e726e28d
chimmimmi <145756968+chimmimmi@users.noreply.github.com>
2024-01-02 20:49:23 +0900

    2024-01-02 (20:49)
    메인화면(list.html)의 위치 잘못된 일부분 수정
    글 쓰기 부분에서 "보험종목" 부분 명단 추가
    
    Chapter 18. REST 방식 댓글 등록
    
    1) CommentController 에 'registerComment' 메서드 구현
    
    2) view.html의 댓글 영역인 'add-content' 프래그먼트의 <form> 태그 구조 변경
    
    3) view.html의 자바스크립트 영역 'insertComment' 함수 구현

--- src/main/java/com/insurance/controller/CommentController.java
+++ src/main/java/com/insurance/controller/CommentController.java
@@ -3,11 +3,16 @@ package com.insurance.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonObject;
 import com.insurance.domain.CommentDTO;
 import com.insurance.service.CommentService;
 
@@ -16,6 +21,39 @@ public class CommentController
 {
 	@Autowired
 	private CommentService commentService;
+	
+	// 게시글의 경우, 하나의 URI로 생성(INSERT)과 수정(UPDATE) 처리가 가능하지만, 'REST API'는 설계 규칙을 지켜야 하기 때문에 URI를 구분함
+	 // value "/comments" :: 새로운 댓글 등록, "/comments/{idx} :: 댓글 테이블의 PK인 댓글 번호(idx)에 해당하는 댓글 수정
+	  // RequestMethod.POST  :: HTTP 요청 메서드 중 POST를 의미하며, @PostMapping과 유사
+	  // RequestMethod.PATCH :: HTTP 요청 메서드 중 PATCH를 의미하며, @PatchMapping과 유사 
+	@RequestMapping(value = { "/comments", "/comments/{idx}" }, method = {RequestMethod.POST, RequestMethod.PATCH })
+	// RequestBody :: REST 방식의 처리에 사용. 파라미터 앞에 @RequestBody가 지정되면, 파라미터로 전달받은 JSON 문자열을 객체로 변환
+	 // 클라이언트(사용자)는 게시글 번호, 댓글 내용, 댓글 작성자를 JSON 문자열로 전송한다.
+	 // 서버(컨트롤러)는 JSON 문자열을 파라미터로 전달받는다.
+	 // @RequestBody는 전달받은 JSON 문자열을 객체로 변환한다.
+	 // 객체로 변환된 JSON은 CommentDTO 클래스의 객체인 params에 매핑(바인딩)된다.
+	public JsonObject registerComment(@PathVariable(value = "idx", required = false) Long idx, @RequestBody final CommentDTO params)
+	{                                                                                                                                   
+		JsonObject jsonobj = new JsonObject();
+		
+		try
+		{
+			// 결과를 저장할 JOSN 객체 생성. idx를 파라미터로 전달받으면 댓글 수정
+			// isRegistered
+			// CommentService 의 registerComment 메서드를 실행한 결과 저장
+			// 댓글의 생성 또는 수정이 실행되면 true를, 실행되지 않으면 false를 저장
+			// 메서드의 실행 결과를 "result" 라는 이름의 프로퍼터로 JSON 객체에 추가해서 리턴
+			boolean isRegistered = commentService.registerComment(params);
+			jsonobj.addProperty("result", isRegistered);
+			
+		} catch (DataAccessException e) {
+			jsonObj.addProperty("message", "데이터베이스 처리 과정에 문제가 발생하였습니다.");
+			
+		} catch (Exception e) {
+			jsonObj.addProperty("message", "시스템에 문제가 발생하였습니다.");
+		}
+		return jsonObj;
+	}
 	
 	@GetMapping(value = "/comments/{insuranceIdx}")
 	public List<CommentDTO> getCommentList(@PathVariable("insuranceIdx") Long insuranceIdx, @ModelAttribute("params") CommentDTO params)   // @PathVariable :: @RequestParam과 유사한 기능을 하며, REST 방식에서 리소를 표현할 때 사용
